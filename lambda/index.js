3c1a4c0245220f1376e5cd543b6feb30d59338bd
ivan <ivansu.webmail@gmail.com>
2018-10-18 11:40:01 +0700

    init app

---
+++ lambda/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+console.log('Loading function');
+
+const aws = require('aws-sdk');
+
+// aws.config.update({ 
+//   "accessKeyId": "AKIAI2JUHNLGQ6GTCCEA", 
+//   "secretAccessKey": "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", 
+//   "region": "us-east-1"
+// });
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+exports.handler = (event, context, callback) => {
+    // console.log('Received event:', JSON.stringify(event, null, 2));
+    // console.log(event, '<======================= EVENT');
+
+    let bucketName = event["Records"][0]["s3"]["bucket"]["name"];
+    let fileName = event["Records"][0]["s3"]["object"]["key"];
+
+    return {
+      bucketName, fileName
+    }
+
+    // Get the object from the event and show its content type
+    // const bucket = event.Records[0].s3.bucket.name;
+    // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    // const params = {
+    //     Bucket: bucket,
+    //     Key: key,
+    // };
+    // s3.getObject(params, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+    //         console.log(message);
+    //         callback(message);
+    //     } else {
+    //         console.log('CONTENT TYPE:', data.ContentType);
+    //         callback(null, data.ContentType);
+    //     }
+    // });
+};
