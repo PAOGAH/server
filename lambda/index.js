37b1602f35da9bf3614081b03ea54dd383f7d3d7
ivan <ivansu.webmail@gmail.com>
2018-10-18 14:21:50 +0700

    refactor code and added some comment

--- lambda/index.js
+++ lambda/index.js
@@ -11,17 +11,12 @@ AWS.config.update({
   "region": "us-east-1"
 });
 
-// const s3 = new aws.S3({ apiVersion: '2006-03-01' });
-
 exports.handler = (event, context, callback) => {
-    // console.log('Received event:', JSON.stringify(event, null, 2));
-    // console.log(event, '<======================= EVENT');
+
+    console.log('Get bucket meta data');
 
     let bucketName = event["Records"][0]["s3"]["bucket"]["name"];
     let fileName = event["Records"][0]["s3"]["object"]["key"];
-
-    console.log(bucketName, '<=============== BUCKET NAME');
-    console.log(fileName, '<============= FILE NAME');
 
     const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
 
@@ -35,15 +30,18 @@ exports.handler = (event, context, callback) => {
     }
     
     rekognition.detectText(params, function(err, data) {
+      console.log('Get image labels');
+
       if (err) {
         console.log(err, err.stack);
       }
       else {
+        console.log('Want to insert to database');
+
         const platCriteria = /[a-z]+\s[0-9]+\s[a-z]+/i
     
         let detectedText = data.TextDetections.map(detected => detected.DetectedText); 
         let plat = detectedText.find(platText => platCriteria.test(platText));
-        console.log(plat, '<=========== PLAT CODE');
     
         db
           .ref('/plat')
@@ -65,23 +63,4 @@ exports.handler = (event, context, callback) => {
       bucketName,
       fileName
     }
-
-    // Get the object from the event and show its content type
-    // const bucket = event.Records[0].s3.bucket.name;
-    // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    // const params = {
-    //     Bucket: bucket,
-    //     Key: key,
-    // };
-    // s3.getObject(params, (err, data) => {
-    //     if (err) {
-    //         console.log(err);
-    //         const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-    //         console.log(message);
-    //         callback(message);
-    //     } else {
-    //         console.log('CONTENT TYPE:', data.ContentType);
-    //         callback(null, data.ContentType);
-    //     }
-    // });
 };
