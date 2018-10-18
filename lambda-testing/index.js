5d3840768afc6684d60cf7974c97b6764ac7054c
ivan <ivansu.webmail@gmail.com>
2018-10-18 17:14:41 +0700

    backup index.js

--- lambda-testing/index.js
+++ lambda-testing/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('Loading function');
-
 const AWS = require('aws-sdk');
 const { db } = require('./firebase.config');
 
@@ -11,56 +9,56 @@ AWS.config.update({
   "region": "us-east-1"
 });
 
-exports.handler = (event, context, callback) => {
+// exports.handler = (event, context, callback) => {
 
-    console.log('Get bucket meta data');
+//     console.log('Get bucket meta data');
 
-    let bucketName = event["Records"][0]["s3"]["bucket"]["name"];
-    let fileName = event["Records"][0]["s3"]["object"]["key"];
+//     let bucketName = event["Records"][0]["s3"]["bucket"]["name"];
+//     let fileName = event["Records"][0]["s3"]["object"]["key"];
 
-    const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+//     const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
 
-    let params = {
-      "Image": {
-          "S3Object": {
-              "Bucket": bucketName,
-              "Name": fileName
-          }
-      }
-    }
+//     let params = {
+//       "Image": {
+//           "S3Object": {
+//               "Bucket": bucketName,
+//               "Name": fileName
+//           }
+//       }
+//     }
     
-    rekognition.detectText(params, function(err, data) {
-      console.log('Get image labels');
+//     rekognition.detectText(params, function(err, data) {
+//       console.log('Get image labels');
 
-      if (err) {
-        console.log(err, err.stack);
-      }
-      else {
-        console.log('Want to insert to database');
+//       if (err) {
+//         console.log(err, err.stack);
+//       }
+//       else {
+//         console.log('Want to insert to database');
 
-        const platCriteria = /[a-z]+\s[0-9]+\s[a-z]+/i
+//         const platCriteria = /[a-z]+\s[0-9]+\s[a-z]+/i
     
-        let detectedText = data.TextDetections.map(detected => detected.DetectedText); 
-        let plat = detectedText.find(platText => platCriteria.test(platText));
+//         let detectedText = data.TextDetections.map(detected => detected.DetectedText); 
+//         let plat = detectedText.find(platText => platCriteria.test(platText));
     
-        db
-          .ref('/plat')
-          .push({
-            text: plat,
-            status: 1,
-            createdAt: new Date().toDateString()
-          }, (err) => {
-            if (err) {
-              console.error(err);
-            } else {
-              console.log('inserted to firebase');
-            }
-          });
-      }
-    });
+//         db
+//           .ref('/plat')
+//           .push({
+//             text: plat,
+//             status: 1,
+//             createdAt: new Date().toDateString()
+//           }, (err) => {
+//             if (err) {
+//               console.error(err);
+//             } else {
+//               console.log('inserted to firebase');
+//             }
+//           });
+//       }
+//     });
 
-    return {
-      bucketName,
-      fileName
-    }
-};
+//     return {
+//       bucketName,
+//       fileName
+//     }
+// };
