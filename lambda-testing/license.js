7d77e4de4203fac8237e34634119c1bf900a741c
ivan <ivansu.webmail@gmail.com>
2018-10-24 16:14:04 +0700

    license controller

---
+++ lambda-testing/license.js
@@ -0,0 +1,31 @@
+const { firestore } = require('./firebase.config');
+
+firestore.settings({ timestampsInSnapshots: true });
+
+module.exports = {
+  getByLicense (plat) {
+    return new Promise((resolve, reject) => {
+      firestore
+        .collection('licenses')
+        .where('text', '==', plat)
+        .where('status', '==', true)
+        .get()
+        .then(snapshot => {
+          resolve(snapshot);
+        })
+    });
+  },
+
+  createLicense (plat, s3) {
+    firestore.collection('licenses').add({
+      text: plat,
+      status: true,
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
+      imgTrue: `https://s3.amazonaws.com/${s3.bucketName}/${s3.fileName}`,
+      imgFalse: ''
+    }).then((doc) => {
+        // callback(null, { id: doc.id, plat: plat});
+      });
+  }
+}
